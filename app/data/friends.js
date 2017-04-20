003910756ba471f7771ff499a674223706f108ba
Taylor Harvey <Taylor Harvey>
2017-04-19 23:11:12 -0400

    2/3rds done

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+/*friends data ===============================================================*/
+var friendsArray = [
+  {
+  "name":"Taylor",
+  "photo":"https://scontent-mia1-2.xx.fbcdn.net/v/t1.0-9/17264917_1717294014954430_7990486607606240626_n.jpg?oh=acc7c89d16fc0df093d30e3d56776769&oe=597C3B86",
+  "scores":[
+      5,
+      3,
+      3,
+      1,
+      3,
+      4,
+      4,
+      2,
+      2,
+      4
+    ]
+  }, {
+     "name":"Brian",
+  "photo":"https://scontent-mia1-2.xx.fbcdn.net/v/t31.0-8/15235507_10153922878255689_7274628622253719340_o.jpg?oh=e3566f490a03989cc820cd58d2339f0f&oe=5985CBE3",
+  "scores":[
+      5,
+      4,
+      4,
+      2,
+      4,
+      2,
+      3,
+      3,
+      3,
+      4
+    ]
+  }, {
+    "name":"Jennifer",
+  "photo":"https://scontent-mia1-2.xx.fbcdn.net/v/t1.0-9/12548836_1162906340401101_4391707339725380808_n.jpg?oh=3e17cad7782cbe39c3a3cdfc9b49885d&oe=59871932",
+  "scores":[
+      3,
+      2,
+      2,
+      2,
+      5,
+      4,
+      3,
+      3,
+      4,
+      4
+    ]
+  },
+];
+
+// Makes it accessible to other files using require.
+module.exports = friendsArray;
+
+
